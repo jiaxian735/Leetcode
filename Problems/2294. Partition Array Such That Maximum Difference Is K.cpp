ddae8bc4a9c128e00c773969ce6bd397fa35ca9f
jiaxian735 <95913567+jiaxian735@users.noreply.github.com>
2025-06-20 21:32:09 +0800

    Update 2294. Partition Array Such That Maximum Difference Is K.cpp

--- Problems/2294. Partition Array Such That Maximum Difference Is K.cpp
+++ Problems/2294. Partition Array Such That Maximum Difference Is K.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
-    int partitionArray(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int ans = 1, t = nums[0];
+    int partitionArray(const vector<int>& nums, int k) {
+        bool n[100001] = {};
+        int ans = 1, i = 1e5 + 1, j = 0;
         for (int x : nums) {
-            if (x - t > k)
-                ans++, t = x;
+            n[x] = 1;
+            i = min(i, x);
+            j = max(j, x);
+        }
+        i += k + 1;
+        while (i <= j) {
+            if (n[i]) {
+                ans++, i += k + 1;
+            } else
+                i++;
         }
         return ans;
     }
