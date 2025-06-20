adb6b6b0926ba6033257b499e774e672298eb61e
jiaxian735 <95913567+jiaxian735@users.noreply.github.com>
2025-06-20 21:24:51 +0800

    Update 1. Two Sum.cpp

--- Problems/1. Two Sum.cpp
+++ Problems/1. Two Sum.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>m;
-        for(int i=0;i<nums.size();i++){
-            int t=target-nums[i];
-            if(m.find(t)!=m.end())return{m[t],i};
-            m[nums[i]]=i;
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            int t = target - nums[i];
+            if (m.find(t) != m.end())
+                return {m[t], i};
+            m[nums[i]] = i;
         }
         return {};
     }
